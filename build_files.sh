40a53a9f4f77ba7346e5e0ffd4590336fbdd3dc4
haris <harissaeed795@gmail.com>
2023-06-17 18:58:06 +0500

    initial commit

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,3 @@
 echo " BUILD START"
-python3.9 -m pip install -r requirements.txt
-python3.9 manage.py collectstatic --noinput --clear
+python3.11.3 manage.py collectstatic --noinput --clear
 echo " BUILD END"
